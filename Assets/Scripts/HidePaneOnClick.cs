d1ea0e3462452019da88d321b2c54df82de954b2
David Bohan <dbohan@apterainc.com>
2016-11-02 16:30:37 -0400

    Add hide/show button on tagalong

---
+++ Assets/Scripts/HidePaneOnClick.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HidePaneOnClick : MonoBehaviour {
+    public GameObject PaneToHide;
+    private bool IsVisible;
+
+    void Start()
+    {
+        IsVisible = false;
+    }
+
+    void OnSelect()
+    {
+        IsVisible = !IsVisible;
+        PaneToHide.SetActive(IsVisible);
+        if (IsVisible)
+        {
+            gameObject.GetComponentInChildren<Text>().text = "Hide";
+        } else
+        {
+            gameObject.GetComponentInChildren<Text>().text = "Show";
+        }
+    }
+
+    public bool IsTargetObjectHiding()
+    {
+        return IsVisible;
+    }
+}
